ce897aebca89b90f4c362f563e94f9e0ccc04149
Oscar Söderlund <odsod@users.noreply.github.com>
2022-01-31 09:09:22 +0100

    ci: bump sage

--- .sage/sagefile.go
+++ .sage/sagefile.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 
-	"github.com/go-logr/logr"
 	"go.einride.tech/sage/sg"
 	"go.einride.tech/sage/sgtool"
 	"go.einride.tech/sage/tools/sgconvco"
@@ -31,53 +30,53 @@ func All(ctx context.Context) error {
 }
 
 func FormatYAML(ctx context.Context) error {
-	logr.FromContextOrDiscard(ctx).Info("formatting YAML files...")
-	return sgyamlfmt.FormatYAML(ctx)
+	sg.Logger(ctx).Println("formatting YAML files...")
+	return sgyamlfmt.Command(ctx, "-d", sg.FromGitRoot(), "-r").Run()
 }
 
 func GoModTidy(ctx context.Context) error {
-	logr.FromContextOrDiscard(ctx).Info("tidying Go module files...")
+	sg.Logger(ctx).Println("tidying Go module files...")
 	return sg.Command(ctx, "go", "mod", "tidy", "-v").Run()
 }
 
 func GoTest(ctx context.Context) error {
-	logr.FromContextOrDiscard(ctx).Info("running Go tests...")
+	sg.Logger(ctx).Println("running Go tests...")
 	return sggo.TestCommand(ctx).Run()
 }
 
 func GoReview(ctx context.Context) error {
-	logr.FromContextOrDiscard(ctx).Info("reviewing Go files...")
+	sg.Logger(ctx).Println("reviewing Go files...")
 	return sggoreview.Command(ctx, "-c", "1", "./...").Run()
 }
 
 func GolangciLint(ctx context.Context) error {
-	logr.FromContextOrDiscard(ctx).Info("linting Go files...")
+	sg.Logger(ctx).Println("linting Go files...")
 	return sggolangcilint.Run(ctx)
 }
 
 func FormatMarkdown(ctx context.Context) error {
-	logr.FromContextOrDiscard(ctx).Info("formatting Markdown files...")
+	sg.Logger(ctx).Println("formatting Markdown files...")
 	return sgmarkdownfmt.Command(ctx, "-w", ".").Run()
 }
 
 func ConvcoCheck(ctx context.Context) error {
-	logr.FromContextOrDiscard(ctx).Info("checking git commits...")
+	sg.Logger(ctx).Println("checking git commits...")
 	return sgconvco.Command(ctx, "check", "origin/master..HEAD").Run()
 }
 
 func GitVerifyNoDiff(ctx context.Context) error {
-	logr.FromContextOrDiscard(ctx).Info("verifying that git has no diff...")
+	sg.Logger(ctx).Println("verifying that git has no diff...")
 	return sggit.VerifyNoDiff(ctx)
 }
 
 func Stringer(ctx context.Context) error {
-	logr.FromContextOrDiscard(ctx).Info("building...")
+	sg.Logger(ctx).Println("building...")
 	_, err := sgtool.GoInstall(ctx, "golang.org/x/tools/cmd/stringer", "v0.1.8")
 	return err
 }
 
 func GoGenerate(ctx context.Context) error {
 	sg.Deps(ctx, Stringer)
-	logr.FromContextOrDiscard(ctx).Info("generating Go code...")
+	sg.Logger(ctx).Println("generating Go code...")
 	return sg.Command(ctx, "go", "generate", "./...").Run()
 }
